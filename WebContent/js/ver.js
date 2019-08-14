0b1c1c20cd0bdd393cb9e76cea46fff2b68c3440
Arian Fornaris <developers@phasereditor2d.com>
2019-08-13 23:39:05 -0400

    Get last changes from Chains.

--- WebContent/js/ver.js
+++ WebContent/js/ver.js
@@ -1,6 +1,6 @@
 var Chains;
 (function (Chains) {
-    Chains.VERSION = 2;
-    Chains.SEMVER = "2.0.0";
+    Chains.VERSION = 3;
+    Chains.SEMVER = "2.0.1";
     Chains.PHASER_VERSION = "3.19.0";
 })(Chains || (Chains = {}));
--- WebContent/ts/ver.ts
+++ WebContent/ts/ver.ts
@@ -1,5 +1,5 @@
 namespace Chains {
-    export const VERSION = 2;
-    export const SEMVER = "2.0.0";
+    export const VERSION = 3;
+    export const SEMVER = "2.0.1";
     export const PHASER_VERSION = "3.19.0";
 }
