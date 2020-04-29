44ca9eb4d4a9231e3bb8df5917f5febda6118160
Arian Fornaris <developers@phasereditor2d.com>
2020-04-28 23:26:35 -0400

    Update to Phaser 3.23.0.

--- WebContent/js/ver.js
+++ WebContent/js/ver.js
@@ -1,6 +1,6 @@
 var Chains;
 (function (Chains) {
-    Chains.VERSION = 5;
-    Chains.SEMVER = "2.0.3";
-    Chains.PHASER_VERSION = "3.21.0";
+    Chains.VERSION = 6;
+    Chains.SEMVER = "2.0.4";
+    Chains.PHASER_VERSION = "3.23.0";
 })(Chains || (Chains = {}));
--- WebContent/ts/ver.ts
+++ WebContent/ts/ver.ts
@@ -1,5 +1,5 @@
 namespace Chains {
-    export const VERSION = 5;
-    export const SEMVER = "2.0.3";
-    export const PHASER_VERSION = "3.21.0";
+    export const VERSION = 6;
+    export const SEMVER = "2.0.4";
+    export const PHASER_VERSION = "3.23.0";
 }
