1bb10ea64e6783da0f58f04a4599202d2f96762c
Arian Fornaris <developers@phasereditor2d.com>
2020-07-15 10:55:25 -0400

    Missing compilation.

--- WebContent/js/ver.js
+++ WebContent/js/ver.js
@@ -1,6 +1,6 @@
 var Chains;
 (function (Chains) {
-    Chains.VERSION = 6;
-    Chains.SEMVER = "2.0.4";
-    Chains.PHASER_VERSION = "3.23.0";
+    Chains.VERSION = 7;
+    Chains.SEMVER = "2.0.5";
+    Chains.PHASER_VERSION = "3.24.1";
 })(Chains || (Chains = {}));
